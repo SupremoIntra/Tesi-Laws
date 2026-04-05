d1be635f74e31c87dfefa962cb8a51cda8aa6ffb
Daniele Intra <daniele.intra@studenti.unimi.it>
2026-04-05 18:37:19 +0200

    finito webcam con confidence e class e iniziato VisDrone

--- YOLO/Webcam/webcam.py
+++ YOLO/Webcam/webcam.py
@@ -6,26 +6,34 @@ cap = cv2.VideoCapture(0) # 0 per la webcam integrata
 cap.set(3, 1280) #  width
 cap.set(4, 720) # height
 
-model = YOLO("../Yolo-Weights/yolov8l.pt") # Carica il modello YOLOv8 nano
+model = YOLO("../Yolo-Weights/yolov8l.pt") # Carica il modello YOLOv8 
 
 while True:
     success, img = cap.read() # Legge un frame dalla webcam
     results = model(img, stream=True) # Esegui il rilevamento sull'immagine, stream=True per ottenere i risultati in tempo reale, più efficiente
     
     for r in results:
-        boxes = r.boxes # Ottieni le bounding box
+        boxes = r.boxes # Ottieni le bounding box dai risultati
         for box in boxes:
-            
-            #PRIMO METODO OPENCV
+            #usiamo CVzone (più easy di OpenCV)
             x1, y1, x2, y2 = box.xyxy[0] # Ottieni le coordinate della bounding box
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2) # Converti le coordinate in interi (per disegnare i rettangoli con OpenCV)
-            #cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3) # Disegna un rettangolo intorno all'oggetto rilevato, do l'img, le coordinate del rettangolo, il colore (in questo caso rosa) e lo spessore del rettangolo
             
             
-            #SECONDO METODO CVZONE
-            w, h = x2 - x1, y2 - y1 # Calcola la larghezza e l'altezza del rettangolo
+            #calcola la confidenza
+            conf = box.conf[0].item() # Ottieni la confidenza del rilevamento
+            cls = box.cls[0].item() # Ottieni la classe dell'oggetto
             
-            cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255)) # Disegna un rettangolo con angoli arrotondati intorno all'oggetto rilevato, do l'img, le coordinate del rettangolo, la larghezza e l'altezza del rettangolo, la lunghezza degli angoli arrotondati, il raggio degli angoli arrotondati e il colore (in questo caso rosa)
-    
+            if conf >0.80 :
+                 cvzone.cornerRect(img, (x1, y1, x2-x1, y2-y1), l=30, rt=2, colorR=(0, 255, 0)) 
+                 
+                 cvzone.putTextRect(img, f"{model.names[int(cls)]} {conf:.2f}", (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=3) # Disegna il nome della classe e la confidenza sopra la bounding box
+                 
+            else:
+                cvzone.cornerRect(img, (x1, y1, x2-x1, y2-y1), l=30, rt=2, colorR=(0, 0, 255)) 
+                
+                cvzone.putTextRect(img, f"{model.names[int(cls)]} {conf:.2f}", (max(0, x1), max(35, y1)), scale=1, thickness=1, offset=3) # Disegna il nome della classe e la confidenza sopra la bounding box
+                
+            
     cv2.imshow("Webcam", img)
     cv2.waitKey(1)
